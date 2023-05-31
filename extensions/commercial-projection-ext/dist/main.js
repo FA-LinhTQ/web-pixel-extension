a74d77c6ade15b66b6f83047073aab17c9a0f3a3
Linh Tran <linhtq1@firegroup.io>
2023-05-31 15:48:14 +0700

    init

---
+++ extensions/commercial-projection-ext/dist/main.js
@@ -0,0 +1,1 @@
+(()=>{var o="WebPixel::Render";var e=function(r){return shopify.extend(o,r)};e(({configuration:t,analytics:r,browser:n})=>{r.subscribe("all_events",i=>{console.log("Event",i)})});})();
